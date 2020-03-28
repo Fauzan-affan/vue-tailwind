f2b31334de52d0bd3cc61d21cf777272dfb5c6c9
Fauzan-affan <fauzanaffanzaki93@gmail.com>
2020-03-28 19:15:07 +0700

    vue-tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" class="flex bg-gray-900 text-white min-h-screen">
+
+    <SidebarNavigation />
+
+    <div class="w-full ml-24">
+      <transition name="page">
+        <router-view/>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+import SidebarNavigation from "@/components/SidebarNavigation.vue";
+
+export default {
+  components: {
+    SidebarNavigation
+  }
+}
+</script>
+
+<style src="./css/main.css"/>
